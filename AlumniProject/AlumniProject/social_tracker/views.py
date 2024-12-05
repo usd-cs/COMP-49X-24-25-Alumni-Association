9d9618d69ecc99c775b52b16269ba0c1efef0a24
audreynaidu <anaidu@sandiego.edu>
2024-12-05 00:08:30 -0800

    added option to save access token, error messages for invalid tokens.

--- AlumniProject/AlumniProject/social_tracker/views.py
+++ AlumniProject/AlumniProject/social_tracker/views.py
@@ -1,8 +1,35 @@
 from django.http import JsonResponse
 from .utils.get_instagram_data import get_instagram_posts
 from .utils.write_database_to_csv import export_posts_to_csv
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+ACCESS_TOKEN = None
+
+@csrf_exempt
+def save_access_token(request):
+    """
+    Saves the provided access token.
+    """
+    global ACCESS_TOKEN
+    if request.method != "POST":
+        #bad request
+        return JsonResponse({"message": "Invalid request."}, status=405)
+
+    try:
+        data = json.loads(request.body)
+        ACCESS_TOKEN = data.get("access_token")
+        if ACCESS_TOKEN:
+            #success message
+            return JsonResponse({"message": "Access token saved successfully."})
+        #blank text box
+        return JsonResponse({"message": "Access token required."}, status=400)
+    except json.JSONDecodeError:
+        #invalid token
+        return JsonResponse({"message": "Invalid token."}, status=400)
+
+
 def get_posts_view(request):
-    ACCESS_TOKEN = "IGQWRNVkJNS0UycG5obmY5ekxUV1VwRkVWVVV3R0dwN29CNkc2ZA3VDa1F5cFBWR2QycU9fRVFjaDVhYTAwdXBRcUlWY1pCZA3lBSGJ4a3JPVklHZAy1QWmVocGM5bnNoVVBEYzRydEJIVGdFN2ZA6NHA3Q3dyRTAzc1EZD"
     result = get_instagram_posts(ACCESS_TOKEN)
     return JsonResponse({'message': result})
 
