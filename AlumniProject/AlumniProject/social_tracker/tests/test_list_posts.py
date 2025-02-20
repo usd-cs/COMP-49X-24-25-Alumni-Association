68b762e0129e9e83a9164e2667c4bd9e77e70243
audreynaidu <anaidu@sandiego.edu>
2025-02-20 15:40:07 -0800

    modified tests for posts

--- AlumniProject/AlumniProject/social_tracker/tests/test_list_posts.py
+++ AlumniProject/AlumniProject/social_tracker/tests/test_list_posts.py
@@ -19,6 +19,7 @@ class ListStoredPostsTests(TestCase):
             num_comments=20,
             num_shares=5,
             num_saves=10,
+            post_API_ID = 234567,
         )
 
         self.post2 = Post.objects.create(
@@ -28,6 +29,7 @@ class ListStoredPostsTests(TestCase):
             num_comments=10,
             num_shares=2,
             num_saves=5,
+            post_API_ID = 23456789,
         )
 
         self.post3 = Post.objects.create(
@@ -37,6 +39,7 @@ class ListStoredPostsTests(TestCase):
             num_comments=30,
             num_shares=15,
             num_saves=20,
+            post_API_ID = 23456798,
         )
 
     def test_get_all_posts(self):
