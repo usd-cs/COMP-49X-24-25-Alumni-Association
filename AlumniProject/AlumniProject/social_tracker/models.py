3b7cad4d3bf18a41aea9e22bb5485533152c059c
audreynaidu <anaidu@sandiego.edu>
2025-02-20 15:07:11 -0800

    Fixed API so that it gets likes, comments, saves, and shares correctly for posts.

--- AlumniProject/AlumniProject/social_tracker/models.py
+++ AlumniProject/AlumniProject/social_tracker/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     num_comments = models.IntegerField(default=0)
     num_shares = models.IntegerField(default=0)
     num_saves = models.IntegerField(default=0)
+    post_API_ID = models.IntegerField(default=None)
 
     class Meta:
         ordering = ["-date_posted"]
--- AlumniProject/AlumniProject/social_tracker/utils/get_instagram_data.py
+++ AlumniProject/AlumniProject/social_tracker/utils/get_instagram_data.py
@@ -3,16 +3,17 @@ from datetime import datetime
 from social_tracker.models import Post
 
 
-def get_instagram_posts(access_token, num_posts=5):
+def get_instagram_posts(access_token, num_posts=100):
     """
     Gets recent Instagram posts using the provided access token
     and adds them to the database. This function makes a GET request
-    to the Instagram API to retrieve recent post data. Any post not
+    to the Instagram API to retrieve recent post data and then another
+    get request that gets the post attributes with the ID. Any post not
     already in in the database is added.
 
     Parameters:
     - access_token (str): A valid access token for the Instagram API.
-    - num_posts (int): number of recent posts to process (default: 5).
+    - num_posts (int): number of recent posts to process (default: 100).
 
     Returns:
     - str: Message indicating the result of the operation:
@@ -25,10 +26,7 @@ def get_instagram_posts(access_token, num_posts=5):
     """
     url = "https://graph.instagram.com/me/media"
     params = {
-        "fields": (
-            "id, caption,media_url,media_type,"
-            "timestamp,permalink,like_count,comments_count"
-        ),
+        "fields":"id,media_url,timestamp,permalink",
         "access_token": access_token,
     }
 
@@ -45,21 +43,35 @@ def get_instagram_posts(access_token, num_posts=5):
                     post["timestamp"], "%Y-%m-%dT%H:%M:%S%z"
                 ).replace(tzinfo=None)
                 permalink = post.get("permalink", "N/A")
-                num_likes = post.get("like_count", 0)
-                num_comments = post.get("comments_count", 0)
+                api_id = post.get("id", None)
 
-                # check if link already exists
-                if not Post.objects.filter(post_link=permalink).exists():
-                    # add to database if not duplicate post
+                # check if link already exists and add to database if not duplicate post and post exists
+                if not Post.objects.filter(post_link=permalink).exists() and api_id != None:
+                    #make request for insights based on post ID
+                    url = "https://graph.instagram.com/v19.0/" + str(api_id) + "/insights"
+                    params = {
+                        "metric": "likes,comments,saved,shares",
+                        "period": "lifetime",
+                        "access_token": access_token,
+                        }
+                    response = requests.get(url, params=params)
+                    resp_json = response.json()
+                    data = resp_json.get("data")
+                    num_likes = data[0].get('values')[0].get('value')
+                    num_comments = data[1].get('values')[0].get('value')
+                    num_saved = data[2].get('values')[0].get('value')
+                    num_shares = data[3].get('values')[0].get('value')
                     Post.objects.create(
                         date_posted=date,
                         post_link=permalink,
                         num_likes=num_likes,
                         num_comments=num_comments,
+                        num_shares = num_shares,
+                        num_saves = num_saved,
+                        post_API_ID = api_id
                     )
                 else:
-                    print(f"Duplicate post, not added: {permalink}")
-
+                    print(f"Duplicate post or invalid post- not added: {permalink}")
             return "Posts processed successfully."
         else:
             return "No posts found."
