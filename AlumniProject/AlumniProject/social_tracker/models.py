60dda702be63f65e97f21546cef659faedd144fe
Connor Boll <cboll@sandiego.edu>
2025-03-05 13:51:45 -0800

    comments for all the models

--- AlumniProject/AlumniProject/social_tracker/models.py
+++ AlumniProject/AlumniProject/social_tracker/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 
 class Post(models.Model):
+    """
+    Model representing a social media post.
+
+    Attributes:
+        post_ID (AutoField): Unique identifier for the post.
+        date_posted (DateTimeField): The date and time when the post was made.
+        post_link (CharField): A link to the post.
+        num_likes (IntegerField): The number of likes the post has received.
+        num_comments (IntegerField): The number of comments on the post.
+        num_shares (IntegerField): The number of times the post has been shared.
+        num_saves (IntegerField): The number of times the post has been saved.
+        post_API_ID (CharField): A unique identifier for the post in an external API.
+    """
     post_ID = models.AutoField(primary_key=True)
     date_posted = models.DateTimeField(null=True)
     post_link = models.CharField(max_length=100)
@@ -17,11 +30,21 @@ class Post(models.Model):
 
 
 class AccessToken(models.Model):
+    """
+    Model to store an access token for authentication.
+
+    Attributes:
+        token (TextField): The access token string.
+        account_id (TextField): The account ID associated with the token.
+    
+    Methods:
+        save: Ensures there is only one active access token at a time.
+    """
     token = models.TextField()
     account_id = models.TextField(default="None")
 
     def save(self, *args, **kwargs):
-        # there can only be one access token at once
+        # There can only be one access token at a time
         if AccessToken.objects.exists() and not self.pk:
             AccessToken.objects.all().delete()
         super().save(*args, **kwargs)
@@ -31,6 +54,13 @@ class AccessToken(models.Model):
 
 
 class Country(models.Model):
+    """
+    Model representing a country and the number of interactions from that country.
+
+    Attributes:
+        name (TextField): The name of the country.
+        num_interactions (IntegerField): The number of interactions from this country.
+    """
     name = models.TextField()
     num_interactions = models.IntegerField(default=0)
 
@@ -39,6 +69,13 @@ class Country(models.Model):
 
 
 class City(models.Model):
+    """
+    Model representing a city and the number of interactions from that city.
+
+    Attributes:
+        name (TextField): The name of the city.
+        num_interactions (IntegerField): The number of interactions from this city.
+    """
     name = models.TextField()
     num_interactions = models.IntegerField(default=0)
 
@@ -47,6 +84,13 @@ class City(models.Model):
 
 
 class Age(models.Model):
+    """
+    Model representing an age range and the number of interactions from that age group.
+
+    Attributes:
+        age_range (TextField): The age range
+        num_interactions (IntegerField): The number of interactions from this age range.
+    """
     age_range = models.TextField()
     num_interactions = models.IntegerField(default=0)
 
