68f1beaea809c2af5c90f3eb198e3fd69913a56b
Connor Boll <cboll@sandiego.edu>
2025-04-10 15:12:12 -0700

    Now using actual live server after CSRF fix

--- AlumniProject/AlumniProject/social_tracker/tests/endtoend.sh
+++ AlumniProject/AlumniProject/social_tracker/tests/endtoend.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-USERNAME="connor@sandiego.edu"
-PASSWORD="password"
+USERNAME="bill@sandiego.edu"
+PASSWORD="Password"
 
 #Can uncomment these lines if you want to input username/password info, won't be fully automated but may be necessary
 #read -p "Enter username: " USERNAME
 #read -p "Enter password: " PASSWORD
 
-curl -c cookies.txt -s http://127.0.0.1:8000/login -o login_page.html
+curl -c cookies.txt -s https://alumni-association.dedyn.io/login -o login_page.html
 
 CSRF_TOKEN=$(grep 'csrfmiddlewaretoken' login_page.html | \
   sed -nE 's/.*name="csrfmiddlewaretoken" value="([^"]+)".*/\1/p')
 
 
 echo "Got CSRF Token:" $CSRF_TOKEN
-curl -b cookies.txt -c cookies.txt -s -X POST http://127.0.0.1:8000/login \
+curl -b cookies.txt -c cookies.txt -s -X POST https://alumni-association.dedyn.io/login \
   -H "Content-Type: application/x-www-form-urlencoded" \
   --data "csrfmiddlewaretoken=$CSRF_TOKEN&email=$USERNAME&password=$PASSWORD" \
   -e http://127.0.0.1:8000/login
@@ -22,7 +22,7 @@ echo "\nGot cookies:"
 cat cookies.txt
 
 echo "\nAttempting to access /account-info/"
-curl -b cookies.txt -s http://127.0.0.1:8000/account-info/ > final_page.html
+curl -b cookies.txt -s https://alumni-association.dedyn.io/account-info/ > final_page.html
 
 if grep -q "<h1>Account Information</h1>" final_page.html; then
   echo "\nTest passed! Logged in and accessed account-info page"
